a810faf35803505892d514c98f02132788c51383
Georgii Tretiakov <george22021988@gmail.com>
2025-07-16 10:09:13 +0100

    Update args.c

--- philo/args.c
+++ philo/args.c
@@ -27,14 +27,14 @@ static long	spec_atol(char *s)
 	long	res;
 	int		i;
 
-	res = 0;
+	res = 0; // we start with res = 0
 	i = 0;
 	while (s[i] && ft_isspace(s[i]))
 		i++;
 	if (s[i] && (s[i] == '-' || s[i] == '+'))
 	{
 		if (s[i] == '-')
-			return (-2);
+			return (-2); // we do not accept negative values
 		i++;
 	}
 	if (s[i] && !ft_isdigit(s[i]))
@@ -53,7 +53,7 @@ static int	init_data(char **argv, t_data *cafe)
 {
 	cafe->table = malloc(sizeof(t_table));
 	if (cafe->table == NULL)
-		return (cleanup(12, 0, MALLOC, cafe));
+		return (cleanup(12, 0, MALLOC, cafe)); // Zero means we have nothing to free
 	cafe->table->n_philos = spec_atol(argv[1]);
 	cafe->table->t_die = spec_atol(argv[2]);
 	cafe->table->t_eat = spec_atol(argv[3]);
@@ -61,7 +61,7 @@ static int	init_data(char **argv, t_data *cafe)
 	if (argv[5])
 		cafe->table->n_meals = spec_atol(argv[5]);
 	else
-		cafe->table->n_meals = -1;
+		cafe->table->n_meals = -1; // -1 is a flag telling us we do not have the optional argument
 	return (0);
 }
 
@@ -73,7 +73,7 @@ int	add_and_check_arguments(char **argv, t_data *cafe)
 	if (ret)
 		return (ret);
 	if (cafe->table->n_meals == 0)
-		return (cleanup(22, 1, NO_MEALS, cafe));
+		return (cleanup(22, 1, NO_MEALS, cafe)); // 1 means that only table should be freed
 	else if (cafe->table->n_meals == -2)
 		return (cleanup(22, 1, INVALID, cafe));
 	if (cafe->table->n_philos == 0)
@@ -90,3 +90,10 @@ int	add_and_check_arguments(char **argv, t_data *cafe)
 		return (cleanup(22, 1, NO_TIME, cafe));
 	return (0);
 }
+/*
+* Under system load, timers (used by usleep and gettimeofday) become less precise, 
+* and threads may be scheduled with delays that affect execution timing and synchronization. 
+* The smaller the timestamp values, the greater the discrepancy between expected and actual timing, 
+* and the more CPU-intensive the polling becomes. 
+* 60ms is an empirically chosen safe threshold that ensures reliable and consistent behavior across different systems.
+*/
